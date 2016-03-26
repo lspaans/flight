31b5bd46b1a32de2041b2f4d8cf75dbf9c26acdd
Léon Spaans <leons@gridpoint.nl>
2016-03-26 10:30:58 +0100

    Create several functions

--- flight.js
+++ flight.js
@@ -7,37 +7,69 @@ var morgan = require("morgan");
 var mysql = require("mysql");
 var path = require("path");
 
-var config = JSON.parse(fs.readFileSync("config.json"));
-var connection = mysql.createConnection({
-    host: config.mysql.host,
-    user: config.mysql.user,
-    password: config.mysql.password
-});
-var app = express();
 
-app.set("port", process.env.PORT || 9090);
-app.set("views", __dirname + "/view");
-app.set("view engine", "jade");
+var get_config = function(config) {
+    return(JSON.parse(fs.readFileSync("config.json")));
+};
 
-app.use(morgan("dev"));
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(methodOverride());
-app.use(express.static(path.join(__dirname, "public")));
 
-connection.connect(function(err) {
-    if (err) {
-        console.error("Error connection: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
-});
-
-app.get("/", function(req, res) {
-    res.render("index", {
-        text: "flight"
+var get_dbconn = function(config) {
+    var dbconn = mysql.createConnection({
+        host: config.mysql.host,
+        user: config.mysql.user,
+        password: config.mysql.password
     });
-});
 
-http.createServer(app).listen(app.get("port"), function() {
-    console.log("Express server listening on port " + app.get("port"));
-});
+    dbconn.connect(function(err) {
+        if (err) {
+            console.error("Error connection: " + err.stack);
+            return;
+        }
+        console.log("connected as id " + dbconn.threadId);
+    });
+
+    return(dbconn);
+};
+
+
+var get_app = function(config) {
+    var app = express();
+
+    app.set("port", config.http.portT || 9090);
+    app.set("views", __dirname + config.html.views);
+    app.set("view engine", config.html.engine);
+
+    app.use(morgan("dev"));
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(methodOverride());
+    app.use(express.static(path.join(__dirname, "public")));
+
+    app.get("/", function(req, res) {
+        res.render("index", {
+            text: "flight"
+        });
+    });
+
+    return(app);
+};
+
+
+var start_server = function(app) {
+    http.createServer(app).listen(app.get("port"), function() {
+        console.log("Express server listening on port " + app.get("port"));
+    });
+};
+
+
+var main = function() {
+    var config = get_config();
+    var dbconn = get_dbconn(config);
+    var app = get_app(config);
+
+    start_server(app);
+};
+
+
+if (require.main === module) {
+    main();
+}
